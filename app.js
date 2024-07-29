9cb0ac73def7f965cfd0a239490b7cb94d239cfe
Marco Di Summa <mdisumma@yahoo.com>
2024-07-29 11:04:14 +0100

    setting up endpoint

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import recipies from "./recipes.json" assert { type: "json" };
+// console.log(recipies);
 import {
   getRecipes,
   getRecipeByID,
@@ -13,6 +14,42 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+//get request
+app.get("/recipes", (result, response) => {
+  console.log("ok");
+  response.send("ok");
+});
+
+//get by id request
+app.get("/recipes/:id", (result, response) => {
+  console.log("ok");
+  response.send("ok");
+});
+
+//post request (result.body)
+app.post("/recipes", (result, response) => {
+  console.log(result.body);
+  response.send(result.body);
+});
+
+//post request (result.body)
+app.post("/recipes", (result, response) => {
+  console.log(result.body);
+  response.send(result.body);
+});
+
+//patch by id (result.body)
+app.patch("recipes/:id", (result, response) => {
+  console.log(result.body);
+  response.send(result.body);
+});
+
+//patch by id (result.body)
+app.delete("recipes/:id", (result, response) => {
+  console.log(result.body);
+  response.send(result.body);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
